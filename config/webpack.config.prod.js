aa3eba7b238a4969b9c1948a5e090dc4ab015052
lukasz_brainhub <lukasz@brainhub.pl>
2018-11-28 20:59:56 +0100

    Added vendor chunk

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -184,11 +184,10 @@ module.exports = {
         },
       }),
     ],
-    // Automatically split vendor and commons
-    // https://twitter.com/wSokra/status/969633336732905474
-    // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
-    // Keep the runtime chunk seperated to enable long term caching
-    // https://twitter.com/wSokra/status/969679223278505985
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
     runtimeChunk: true,
   },
   resolve: {
